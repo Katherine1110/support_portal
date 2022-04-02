056fbc84c132e63684ff08b6d2f386ae3a0d71eb
Katherine1110 <k.mihalochkina@gmail.com>
2022-04-02 19:12:44 +0300

    add carousel for shelter

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,83 +1,45 @@
-import { Link, NavLink } from 'react-router-dom';
-import Logo from '../images/logo.png';
-import LogoSVG from '../images/LogoSVG.svg';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
     <div className="container">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light nav-tabs ">
+        <div className="container-fluid">
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <NavLink
-                  to="/"
-                  className="nav-link"
-                  activeClassName="selected"
-                  activeStyle={{
-                    fontWeight: 'bold',
-                    color: 'red',
-                  }}
-                  aria-current="page">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link" activeclassname="selected" aria-current="page">
                   Головна
                 </NavLink>
               </li>
-              <li class="nav-item">
-                <NavLink
-                  to="/donation"
-                  activeClassName="selected"
-                  activeStyle={{
-                    fontWeight: 'bold',
-                    color: 'red',
-                  }}
-                  className="nav-link">
+              <li className="nav-item">
+                <NavLink to="/donation" activeclassname="selected" className="nav-link">
                   Задонатити
                 </NavLink>
               </li>
 
-              <li class="nav-item">
-                <NavLink
-                  to="/activity"
-                  activeClassName="selected"
-                  activeStyle={{
-                    fontWeight: 'bold',
-                    color: 'red',
-                  }}
-                  className="nav-link">
+              <li className="nav-item">
+                <NavLink to="/activity" activeclassname="selected" className="nav-link">
                   Наша діяльність
                 </NavLink>
               </li>
-              <li class="nav-item">
-                <NavLink
-                  to="/shelter"
-                  activeClassName="selected"
-                  activeStyle={{
-                    fontWeight: 'bold',
-                    color: 'red',
-                  }}
-                  className="nav-link">
+              <li className="nav-item">
+                <NavLink to="/shelter" activeclassname="selected" className="nav-link">
                   Сховище
                 </NavLink>
               </li>
-              <li class="nav-item">
-                <NavLink
-                  to="/areas-of-activity"
-                  activeClassName="selected"
-                  activeStyle={{
-                    fontWeight: 'bold',
-                    color: 'red',
-                  }}
-                  className="nav-link">
+              <li className="nav-item">
+                <NavLink to="/areas-of-activity" activeclassname="selected" className="nav-link">
                   Завдання та напрямками діяльності Організації
                 </NavLink>
               </li>
@@ -85,38 +47,6 @@ function NavBar() {
           </div>
         </div>
       </nav>
-      {/* <ul className="nav justify-content-center">
-        <li>
-          <Link to="/" className="nav-link active" aria-current="page">
-            <img className="mx-auto d-block" src={Logo} alt="Logo" width={200} height={200} />
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/" className="nav-link active" aria-current="page">
-            Intro
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/donation" className="nav-link">
-            Donation
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/activity" className="nav-link">
-            Activity
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/shelter" className="nav-link">
-            Shelter
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/areas-of-activity" className="nav-link">
-            Areas of activity
-          </Link>
-        </li>
-      </ul> */}
     </div>
   );
 }
--- src/components/Shelter1.jsx
+++ src/components/Shelter1.jsx
@@ -1,21 +1,193 @@
 import Logo from '../images/logo.png';
-import Collage4 from '../images/collage4.png';
+import ShelterPic2 from '../images/shelter/shelter2.png';
+import ShelterPic3 from '../images/shelter/shelter3.png';
+import ShelterPic4 from '../images/shelter/shelter4.png';
+import ShelterPic5 from '../images/shelter/shelter5.png';
+import ShelterPic6 from '../images/shelter/shelter6.png';
+import ShelterPic7 from '../images/shelter/shelter7.png';
+import ShelterPic8 from '../images/shelter/shelter8.png';
+import ShelterPic9 from '../images/shelter/shelter9.png';
+import ShelterPic10 from '../images/shelter/shelter10.png';
+import ShelterPic11 from '../images/shelter/shelter11.png';
+import ShelterPic12 from '../images/shelter/shelter12.png';
+import ShelterPic13 from '../images/shelter/shelter13.png';
+import ShelterPic14 from '../images/shelter/shelter14.png';
+import ShelterPic15 from '../images/shelter/shelter15.png';
+import ShelterPic16 from '../images/shelter/shelter16.png';
 
 function Shelter1() {
   return (
-    <div className="container">
+    <div className="container bg-img">
+      <div
+        id="carouselExampleDark"
+        className="carousel carousel-dark slide"
+        data-bs-ride="carousel">
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="3"
+            aria-label="Slide 4"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="4"
+            aria-label="Slide 5"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="5"
+            aria-label="Slide 6"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="6"
+            aria-label="Slide 7"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="7"
+            aria-label="Slide 8"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="8"
+            aria-label="Slide 9"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="9"
+            aria-label="Slide 10"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="10"
+            aria-label="Slide 11"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="11"
+            aria-label="Slide 12"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="12"
+            aria-label="Slide 13"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="13"
+            aria-label="Slide 14"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="14"
+            aria-label="Slide 15"></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="15"
+            aria-label="Slide 16"></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="2000">
+            <img src={ShelterPic2} className="d-block w-100" alt="ShelterPic2" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic3} className="d-block w-100" alt="ShelterPic3" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic4} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic5} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic6} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic7} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic8} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic9} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic10} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic11} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic12} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic13} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic14} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic15} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={ShelterPic16} className="d-block w-100" alt="ShelterPic4" />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
       <div>
         <img className="mx-auto d-block" src={Logo} alt="Logo" />
       </div>
-
-      {/* Gallery1  */}
-      <div className="card">
-        <div className="card-body">
-          <h2 className="card-title mb-4">Сховище, яке ми створили:</h2>
-          <img className="img-fluid mx-auto d-block" src={Collage4} alt="Shelter" />
-        </div>
-      </div>
-      {/* Gallery1 */}
     </div>
   );
 }

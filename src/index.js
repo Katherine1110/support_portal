5706ffffd437e981950eae4a3303a568b54a8051
Katherine1110 <k.mihalochkina@gmail.com>
2022-04-09 19:48:54 +0300

    upd StrictMode

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
